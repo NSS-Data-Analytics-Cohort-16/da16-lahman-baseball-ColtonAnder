0adee2c1ec309f3efc24cab44fee25666bf40884
colton kamenca <coltonkamenca@gmail.com>
2025-11-04 17:40:11 -0600

    Half way

--- scripts/baseball.sql
+++ scripts/baseball.sql
@@ -1,9 +1,57 @@
 -- ## Lahman Baseball Database Exercise
 
 -- 1. What range of years for baseball games played does the provided database cover?
+-- SELECT 				1933-2016
+-- 	yearid,
+-- 	count(playerid)
+-- FROM allstarfull
+-- GROUP BY yearid
+-- ORDER BY yearid
+-- ;
 -- 2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
+-- SELECT 
+-- 	namefirst,
+-- 	namelast,
+-- 	height,
+-- 	g_all as t_games,
+-- 	name as t_name
+-- FROM people
+-- 	LEFT JOIN appearances
+-- 	USING(playerid)
+-- INNER JOIN teams
+-- 	USING(teamid)
+-- ORDER BY height ASC
+-- LIMIT 1
+-- ;
 -- 3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which Vanderbilt player earned the most money in the majors?
+-- SELECT
+-- 	playerid,
+-- 	schoolid,
+-- 	SUM(salary)::NUMERIC::MONEY AS t_salary
+-- FROM collegeplaying
+-- 	INNER JOIN salaries
+-- 	USING(playerid)
+-- WHERE schoolid LIKE 'vandy'
+-- GROUP BY 
+-- 	playerid,
+-- 	schoolid
+	
+-- ORDER BY t_salary DESC
+-- ;
 -- 4. Using the fielding table, group players into three groups based on their position: label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". Determine the number of putouts made by each of these three groups in 2016.
+-- SELECT
+-- 	SUM(po),
+-- 	CASE
+-- 		WHEN pos = 'OF' THEN 'Outfield'
+-- 		WHEN pos IN ('1B', '2B', '3B', 'SS') THEN 'Infield'
+-- 		WHEN pos IN ('P', 'C') THEN 'Battery'
+-- 		ELSE 'Other'
+-- 	END as positions
+-- FROM fielding
+-- WHERE yearid = 2016
+-- GROUP BY positions
+-- ORDER BY positions ASC
+-- ;
 -- 5. Find the average number of strikeouts per game by decade since 1920. Round the numbers you report to 2 decimal places. Do the same for home runs per game. Do you see any trends?
 -- 6. Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
 -- 7. From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
